0459e0182440e41c67cf515a0bea481035391371
eedu7 <mueedahmad067@gmail.com>
2026-01-03 11:26:08 +0500

    added the api router

--- backend/core/server.py
+++ backend/core/server.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
+from api import router
 
 
 def get_app() -> FastAPI:
-    app_ = FastAPI()
+    app_ = FastAPI(title="MedInsight AI")
+    app_.include_router(router)
 
     return app_
-
